15b901032dd85b0c261cdf0ccba5758ab072af1f
PhuDK <PhuDKHE186439@fpt.edu.vn>
2025-09-24 21:09:56 +0700

    api them sua xoa category cho group

--- src/main/java/com/devido/devido_be/controller/CategoryController.java
+++ src/main/java/com/devido/devido_be/controller/CategoryController.java
@@ -1,9 +1,53 @@
 package com.devido.devido_be.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devido.devido_be.dto.*;
+import com.devido.devido_be.model.Bill;
+import com.devido.devido_be.model.Category;
+import com.devido.devido_be.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> createCategory(@PathVariable String id, @RequestBody CategoryDTO categoryDTO) {
+        try {
+            CategoryDTO category = categoryService.createCategory(id, categoryDTO);
+            CategoryDTO categoryResponse = new CategoryDTO(category.getId(), category.getName());
+            return ResponseEntity.ok(new ApiResponse<>(true, "Category created successfully", categoryResponse));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse<>(false, "Fail to create Category", null));
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable String id, @RequestBody CategoryDTO categoryDTO) {
+        try {
+            Category categoryResponse = categoryService.updateCategory(id, categoryDTO);
+            return ResponseEntity.ok(new ApiResponse<>(true, "Category updated successfully", categoryResponse));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse<>(false, "Fail to update Category", null));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteGroup(@PathVariable String id) {
+        try {
+            categoryService.deleteeCategory(id);
+            return ResponseEntity.ok(new ApiResponse<>(true, "Category deleted successfully", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse<>(false, "Fail to delete Category", null));
+        }
+    }
 }
--- src/main/java/com/devido/devido_be/service/CategoryService.java
+++ src/main/java/com/devido/devido_be/service/CategoryService.java
@@ -1,15 +1,20 @@
 package com.devido.devido_be.service;
 
 import com.devido.devido_be.dto.CategoryDTO;
+import com.devido.devido_be.dto.ExpenseDTO;
 import com.devido.devido_be.dto.GroupDTO;
-import com.devido.devido_be.model.Category;
+import com.devido.devido_be.dto.ShareRatio;
+import com.devido.devido_be.model.*;
 import com.devido.devido_be.other.UUIDGenerator;
 import com.devido.devido_be.repository.CategoryRepository;
 import com.devido.devido_be.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryService {
@@ -33,5 +38,17 @@ public class CategoryService {
         var id = UUIDGenerator.getRandomUUID();
         var category = categoryRepository.save(new Category(id, group.get(), categoryDTO.getName()));
         return new CategoryDTO(category.getId(), category.getCategoryName());
+    }
+
+    public Category updateCategory(String id, CategoryDTO categoryDTO) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category with id " + categoryDTO.getId() + " not found"));
+        category.setCategoryName(categoryDTO.getName());
+        Category updateCategory = categoryRepository.save(category);
+        return categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category with id " + id + " not found"));
+    }
+
+    public void deleteeCategory(String id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category with id " + id + " not found"));
+        categoryRepository.delete(category);
     }
 }
